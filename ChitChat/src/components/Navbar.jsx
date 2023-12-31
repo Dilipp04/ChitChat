d2c571fc0a636b9d72dfc13826c164898c51f354
Dilipp04 <you@example.comdilipp11082004@gmail.com>
2023-12-31 14:22:45 +0530

    react router

--- ChitChat/src/components/Navbar.jsx
+++ ChitChat/src/components/Navbar.jsx
@@ -5,33 +5,34 @@ import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 import logo from "../assets/logo.png"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='rounded-lg bg-white shadow-xl p-2 z-40 min-w-12 rounded-l-lg sm:translate-x-0 text-center flex flex-col' >
-
+            
             <div className="my-3 mx-auto">
-                <a href="/app">
+                <NavLink to="/app">
                     <img className=" w-12 md:w-16 object-cover" src={logo} alt="" />
-                </a>
+                </NavLink>
             </div>
             <div className="grow overflow-y-auto flex flex-col justify-between bg-gray-50 dark:bg-white  ">
                 <ul className="space-y-4 font-small mx-auto">
                     <li>
-                        <a href="/app" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
+                        <NavLink to="/app" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
                             <HomeIcon />
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
+                        <NavLink to="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
                             <NotificationsIcon />
-                        </a>
+                        </NavLink>
                     </li>
 
                     <li>
-                        <a href="/app/users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
+                        <NavLink to="/app/users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
                             <PeopleAltIcon />
-                        </a>
+                        </NavLink>
                     </li>
                    
 
@@ -39,14 +40,14 @@ const Navbar = () => {
                 </ul>
                 <ul className="pt-4 mt-4 space-y-2 font-smal dark:border-gray-700 mx-auto">
                     <li>
-                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
+                        <NavLink to="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
                             <SettingsIcon />
-                        </a>
+                        </NavLink>
                     </li>
                     <li>
-                        <a href="/logout" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
+                        <NavLink to="/logout" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-rblue hover:bg-lgray dark:hover:bg-lblue group">
                             < LogoutIcon />
-                        </a>
+                        </NavLink>
                     </li>
 
                 </ul>
